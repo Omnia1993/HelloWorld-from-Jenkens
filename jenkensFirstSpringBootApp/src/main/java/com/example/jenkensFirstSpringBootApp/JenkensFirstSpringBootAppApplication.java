5025de4fa8d25ca8dfc8659c21e02965cb37e2cd
Omnia <omniaeid266@gmail.com>
2023-11-24 11:44:47 -0600

    jenkensFirstSpring boot App is pushed

---
+++ jenkensFirstSpringBootApp/src/main/java/com/example/jenkensFirstSpringBootApp/JenkensFirstSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkensFirstSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkensFirstSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkensFirstSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ jenkensFirstSpringBootApp/src/test/java/com/example/jenkensFirstSpringBootApp/JenkensFirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkensFirstSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkensFirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
